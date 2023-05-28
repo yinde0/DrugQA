c49a544e3965884cfe1f2a05f1c09e5f4937e29c
Saad Salami <35911370+yinde0@users.noreply.github.com>
2023-05-28 06:46:07 +0100

    Update main.py

--- scr/main.py
+++ scr/main.py
@@ -79,6 +79,9 @@ async def predict(prompt: str, focus: list):
                }
 
     return final_result
+
+if __name__ == "__main__":
+    uvicorn.run("src.main:app", host="0.0.0.0", port=8080, reload=True)
     
     
     
